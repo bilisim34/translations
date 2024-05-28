faf9cc8f77f93be6b5a87c1d82531635ad1de3f2
serkan_ozdes <serkan_ozdes@hotmail.com>
2024-05-28 16:18:12 +0300

    no message

---
+++ tr/vendor/plugins/post-scheduler/tr/post-scheduler.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'name' => ' Gönderi Zamanlayıcı',
+    'publish_date' => 'Yayın tarihi',
+    'date' => 'Tarih',
+    'time' => 'Zaman',
+    'update_publish_time_to_current_time' => 'Yayınlanan zamanı geçerli zamana güncelle',
+];
